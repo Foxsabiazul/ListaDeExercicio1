5dcc8c4dc341d7f47e0052a414e0444640f50181
MARCOS VINICIUS DA SILVA ROSA <rosa.3950@aluno.pr.senac.br>
2023-09-06 15:03:43 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/algotirumos/Algoritimos.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algotirumos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author rosa.3950
+ */
+/*public class Algotirumos {
+
+    public static void main(String[] args)             
+    {  int num1; 
+        int num2;
+        int num3;
+        int soma; 
+                
+        Scanner scanner = new Scanner(System.in);
+
+       System.out.println("Informe o segundo numero"); 
+       
+        int opçao;
+        
+        float num1, num2, num3, resultado; 
+        
+        Scanner scanner = new Scanner(System.in);
+        String nome; 
+        System.out.println("Informe a operação"); 
+        System.out.println("1 - SOMA"); 
+        System.out.println("2 - SUBTRAÇÃO"); 
+        System.out.println("3 - MULTIPLICAÇÃO"); 
+        System.out.println("4 - DIVISÃO");
+        opcao "" scanner.nextInt();
+        
+        if(opcao == 1 ) {//Soma
+       
+         nome = scanner.next()
+   
+        
+        System.out.println("-------SOMA-------"); 
+        System.out.println("Informe o primeiro numero");
+        num2 = scanner.nextFloat(); 
+         
+
+        System.out.println("Informe o segundo numero");
+        num3 = (int) scanner.nextFloat();
+          
+        resultado = num1 + num2 + num3; 
+        
+        System.out
+     
+        if (nome.equals("Marcos")){
+            System.out.println("Usuário autorizado"); 
+        }else{
+            System.out.println("Usuário NÃO autorizado"); 
+        } 
+       
+    }             
+}*/
